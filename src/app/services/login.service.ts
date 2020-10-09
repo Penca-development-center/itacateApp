f5e763675e76799f796653edcedc49240dede820
peca-develpment <felipe@penca.com.mx>
2020-10-09 10:07:54 -0500

    Agregando nuevas imagenes

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Storage } from "@ionic/storage";
+import { ToastController } from "@ionic/angular";
 @Injectable({
   providedIn: "root",
 })
 export class LoginService {
-  constructor(private http: HttpClient, private storage: Storage) {}
+  constructor(private http: HttpClient, private storage: Storage, private toastCtrl: ToastController) { }
+  
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1700
+    });
+
+    await toast.present();
+  }
 
   doLogIn(userCredentials) {
     return new Promise((accept, reject) => {
@@ -23,10 +33,13 @@ export class LoginService {
           if (loginToken) {
             accept(loginToken);
             console.log(loginToken);
+            this.presentToast(loginToken.id_usuario);
           } else {
             reject({
               message: "Login incorrecto",
             });
+
+            this.presentToast("servicio no disponible, intente mas tarde");
           }
         });
     });
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-
+import { ToastController } from "@ionic/angular";
 @Injectable({
   providedIn: "root",
 })
 export class RegisterService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private toastCtrl: ToastController) {}
+
+  async presentToast(message: string) { 
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1700
+    });
+
+    await toast.present();
+  }
 
   checkUser(userInfo: any) {
     return new Promise((accept, reject) => {
@@ -34,8 +43,10 @@ export class RegisterService {
                 code: 1506,
                 user_id: usuario.id_usuario,
               });
+                               this.presentToast("Usuario existente");
             } else if (usuario.message) {
               accept({ message: usuario.message, code: 1507 });
+              this.presentToast("Usuario registrado");
             }
           },
           (error: any) => {
@@ -64,6 +75,7 @@ export class RegisterService {
           (response: any) => {
             if (response) {
               accept(response);
+
             } else {
               reject({ message: "Error intente más tarde." });
             }
